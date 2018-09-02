643dfff5886cad2b2a3e728f1ff46a730d9a3b65
Jed <jed@m6700.local>
2018-09-02 19:07:04 -0400

    jed@m6700: Sun Sep  2 23:07:04 UTC 2018

--- check.sh
+++ check.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+echo \#\#\#\#\#\#\#\#\#\#
+echo \# flake8 \#
+echo \#\#\#\#\#\#\#\#\#\#
 flake8 --statistics --count "$1"
+
+echo ###############
+echo # pycodestyle #
+echo ###############
 pycodestyle --statistics --max-line-length=79 "$1"
+
+echo ##############
+echo # pydocstyle #
+echo ##############
 pydocstyle  --convention=numpy --verbose "$1"
