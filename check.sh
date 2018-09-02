3fe85ba2e3bb4b39ae312bc92a7eb00df3ec21a5
Jed <jed@m6700.local>
2018-09-02 19:08:05 -0400

    jed@m6700: Sun Sep  2 23:08:05 UTC 2018

--- check.sh
+++ check.sh
@@ -4,13 +4,11 @@ echo \#\#\#\#\#\#\#\#\#\#
 echo \# flake8 \#
 echo \#\#\#\#\#\#\#\#\#\#
 flake8 --statistics --count "$1"
-
-echo ###############
-echo # pycodestyle #
-echo ###############
+echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+echo \# pycodestyle \#
+echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
 pycodestyle --statistics --max-line-length=79 "$1"
-
-echo ##############
-echo # pydocstyle #
-echo ##############
+echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#
+echo \# pydocstyle \#
+echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#
 pydocstyle  --convention=numpy --verbose "$1"
--- checkall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-find pydarknet -name "*.py" | xargs -n1 ./check.sh
