a972287b58504cddb62c7c1ce43ce1c008896bba
Jed <jed@m6700.local>
2018-09-02 17:36:58 -0400

    jed@m6700: Sun Sep  2 21:36:58 UTC 2018

--- check.sh
+++ check.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-pycodestyle --statistics --max-line-length=79 $1
-pydocstyle  --convention=numpy --verbose $1
+flake8 --statistics --count "$1"
+pycodestyle --statistics --max-line-length=79 "$1"
+pydocstyle  --convention=numpy --verbose "$1"
