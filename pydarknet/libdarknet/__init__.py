a2c4b5be196818d0bea746553c8aeb262c4461ee
Jed <jed@m6700.local>
2018-09-03 10:55:54 -0400

    jed@m6700: Mon Sep  3 14:55:54 UTC 2018

--- pydarknet/libdarknet/__init__.py
+++ pydarknet/libdarknet/__init__.py
@@ -18,25 +18,8 @@ import ctypes
 
 from functools import wraps
 from ..config import config
+from ..utils import chroot
 import os
-
-# darknet gets angry if you don't use the darknet directory as 'root'.
-def chroot(f):
-    """Changeroot for functions to keep darknet happy."""
-
-    @wraps(f)
-    def wrapper(self, *args, **kwargs):
-        """Wrap Function."""
-        # Get the 'current' directory to the rest of Python.
-        lwd = os.path.abspath(os.path.curdir)
-        # Change root to the root directory of darknet.
-        os.chdir(self.root)
-        # Execute the function that got wrapped.
-        ret = f(self, *args, **kwargs)
-        # Change back to the 'last' working directory.
-        os.chdir(lwd)
-        # Return the function's output.
-        return ret
 
 class Libdarknet(object):
     """Class for the libdarknet shared library.
@@ -85,3 +68,4 @@ class Libdarknet(object):
     def exists(self):
         """Determine if library exists"""
         return os.path.exists(self.lib)
+
