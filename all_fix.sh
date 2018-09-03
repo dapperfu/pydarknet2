b64d513e64f82a9314ff2ba79006162eb8101c30
Jed <jed@m6700.local>
2018-09-03 14:31:05 -0400

    jed@m6700: Mon Sep  3 18:31:05 UTC 2018

--- all_fix.sh
+++ all_fix.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-
 # Fix all files in a given directory (and below)
 
-# Not ready.
+# Find all ".py" files in the pydarknet folder.
+# For each of those files:
+#    -n1: Pass them individually to fix.sh
+#    -P8: Run 8 in parallel
 find pydarknet -name "*.py" | xargs -n1 -P8 ./fix.sh
 exit 0
