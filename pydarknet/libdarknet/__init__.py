9e002ff7501de007960b902343e8220585c7b3d9
Jed <jed@m6700.local>
2018-09-02 21:43:12 -0400

    jed@m6700: Mon Sep  3 01:43:12 UTC 2018

--- pydarknet/libdarknet/__init__.py
+++ pydarknet/libdarknet/__init__.py
@@ -13,3 +13,43 @@ a line by itself, preferably preceded by a blank line.
 """
 
 from ..config import config
+
+from functools import wraps
+from ..config import config
+
+# darknet gets angry if you don't use the darknet directory as 'root'.
+def chroot(f):
+    """Changeroot for functions to keep darknet happy."""
+
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        """Wrap Function."""
+        # Get the 'current' directory to the rest of Python.
+        lwd = os.path.abspath(os.path.curdir)
+        # Change root to the root directory of darknet.
+        os.chdir(self.root)
+        # Execute the function that got wrapped.
+        ret = f(self, *args, **kwargs)
+        # Change back to the 'last' working directory.
+        os.chdir(lwd)
+        # Return the function's output.
+        return ret
+
+class Libdarknet(object):
+    """Class for the libdarknet shared library."""
+
+    def __init__(self, root=None, weight_dir=None):
+        r"""Initialize a darknet object.
+
+        Parameters
+        ----------
+        var1 : array_like
+            Array_like means all those objects -- lists, nested lists, etc. --
+            that can be converted to an array.  We can also refer to
+            variables like `var1`.
+        var2 : int
+            The type above can either refer to an actual Python type
+            (e.g. ``int``), or describe the type of the variable in more
+            detail, e.g. ``(N,) ndarray`` or ``array_like``.
+        long_var_name : {'hi', 'ho'}, optional
+            Choices in brackets, default first when optional.
