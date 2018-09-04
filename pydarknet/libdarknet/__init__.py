4718df9362bcb49c52a2b3c198e0f470a5255af3
Jed <jed@m6700.local>
2018-09-04 10:55:43 -0400

    jed@m6700: Tue Sep  4 14:55:43 UTC 2018

--- pydarknet/libdarknet/__init__.py
+++ pydarknet/libdarknet/__init__.py
@@ -169,3 +169,5 @@ class Libdarknet(object):
         num = ctypes.c_int(0)
         pnum = ctypes.pointer(num)
         dets = self.lib.get_network_boxes(network, image.w, image.h, threshold, heir_thresh, None, 0, pnum)
+
+        return num, dets
