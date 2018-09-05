dbb1f7a76bb53a7bcdfdaecc0c2c5f04215a0bed
Jed <jed@m6700.local>
2018-09-04 20:39:14 -0400

    jed@m6700: Wed Sep  5 00:39:14 UTC 2018

--- fix.sh
+++ fix.sh
@@ -4,8 +4,3 @@
 black --py36 --line-length=79 $1
 isort --line-width 79 --multi-line 3 --dont-skip __init__.py $1
 
-COMMIT_MSG="`whoami`@`hostname`: `date --universal`"
-echo
-echo --- Committing ${COMMIT_MSG}  ---
-echo
-git commit --all --message "${COMMIT_MSG}"
