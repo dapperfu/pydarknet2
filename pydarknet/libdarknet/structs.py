eaf3892782d8a9a4cc942de653ef8d6b995e9c16
Jed <jed@m6700.local>
2018-09-05 14:15:35 -0400

    jed@m6700: Wed Sep  5 18:15:35 UTC 2018

--- pydarknet/libdarknet/structs.py
+++ pydarknet/libdarknet/structs.py
@@ -9,29 +9,6 @@ class BaseClass(object):
         return "{}<>".format(self.__class__.__name__)
 
 
-class Detections(object):
-    def __init__(self, num, detections_ptr):
-        self.num = num
-        self.detections_ptr = detections_ptr
-
-    def __repr__(self):
-        return "Detections<{}>".format(self.num)
-
-    def __iter__(self):
-        self._idx = 0
-        return self
-
-    def __next__(self):
-        idx = self._idx
-        if self._idx >= self.num:
-            raise StopIteration
-        else:
-            self._idx += 1
-            return self.detections_ptr[idx]
-
-    def __getitem__(self, index):
-        return self.detections_ptr[index]
-
 
 class ArgumentArgs(ctypes.Structure, BaseClass):
     _fields_ = [
