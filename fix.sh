3270289f497baaafe76bdf5f51e75093685e8136
Jed <jed@m6700.local>
2018-09-04 20:19:30 -0400

    jed@m6700: Wed Sep  5 00:19:30 UTC 2018

--- fix.sh
+++ fix.sh
@@ -3,3 +3,9 @@
 #	 --aggressive --aggressive --aggressive $1
 black --py36 --line-length=79 $1
 isort --line-width 79 --multi-line 3 --dont-skip __init__.py $1
+
+COMMIT_MSG="`whoami`@`hostname`: `date --universal`"
+echo
+echo --- Committing ${COMMIT_MSG}  ---
+echo
+git commit --all --message "${COMMIT_MSG}"
