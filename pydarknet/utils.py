a9a24bab3007147f41cc7e4d845d10afed51ba3b
Jed <jed@m6700.local>
2018-09-05 14:50:08 -0400

    jed@m6700: Wed Sep  5 18:50:08 UTC 2018

--- pydarknet/utils.py
+++ pydarknet/utils.py
@@ -44,6 +44,6 @@ def array_to_image(arr):
     h = arr.shape[1]
     w = arr.shape[2]
     arr = (arr / 255.0).flatten()
-    data = dn.c_array(dn.c_float, arr)
-    im = dn.IMAGE(w, h, c, data)
+    data = c_array(dn.c_float, arr)
+    im = Image(w, h, c, data)
     return im
