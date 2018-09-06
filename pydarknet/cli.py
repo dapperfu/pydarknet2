cf427a938b0b6210c9b2db88a88a44e45e3f5242
Jed <jed@m6700.local>
2018-09-05 21:35:30 -0400

    jed@m6700: Thu Sep  6 01:35:30 UTC 2018

--- pydarknet/cli.py
+++ pydarknet/cli.py
@@ -30,7 +30,7 @@ def darknet():
     default=config["darknet"]["root"],
     help="Darknet root directory",
 )
-def clone(root=default_darknet_root, url):
+def clone(root=config["darknet"]["root"], url=config["darknet"]["clone_url"]):
     """Clone a darknet repository"""
     click.echo("URL: %s" % url)
     click.echo("Root: %s" % root)
