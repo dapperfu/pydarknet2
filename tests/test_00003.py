5d94db44051b7769aee9a4023187692be466b49e
Jed <jed@m6700.local>
2018-09-07 12:39:29 -0400

    .

--- tests/test_00003.py
+++ tests/test_00003.py
@@ -1,4 +1,5 @@
 import pydarknet2
+import subprocess
 
 def test_build_000003_01(darknet_root, clone_url):
     # Clone a darknet directory.
@@ -8,4 +9,16 @@ def test_build_000003_01(darknet_root, clone_url):
 def test_clone_000003_02(darknet_root, clone_url):
     # Clone a darknet directory.
     dn = pydarknet2.Darknet(root=darknet_root)
-    dn.build()
+    dn.build(opencv=False, openmp=False, force=True)
+    out = subprocess.check_output(["ldd", dn.exe]).decode("UTF-8")
+    assert "openmp" not in out
+    assert "opencv" not in out
+
+def test_clone_000003_03(darknet_root, clone_url):
+    # Clone a darknet directory.
+    dn = pydarknet2.Darknet(root=darknet_root)
+    dn.build(opencv=True, openmp=True, force=True)
+    out = subprocess.check_output(["ldd", dn.exe]).decode("UTF-8")
+    assert "openmp" in out
+    assert "opencv" in out
+
