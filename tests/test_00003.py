e75e496daee07dce378fef4020f98a32f1c716ff
Jed <jed@m6700.local>
2018-09-07 12:59:15 -0400

    .

--- tests/test_00003.py
+++ tests/test_00003.py
@@ -1,5 +1,7 @@
 import pydarknet2
 import subprocess
+import os
+import pytest
 
 def test_build_000003_01(darknet_root, clone_url):
     # Clone a darknet directory.
@@ -22,8 +24,10 @@ def test_clone_000003_04(darknet_root, clone_url):
     assert "openmp" in out
     assert "opencv" in out
 
+@pytest.mark.skipif(not os.path.exists("/usr/local/cuda"), reason="Requires CUDA to be installed.")
+@pytest.mark.skipif(not os.path.exists("/usr/local/cuda/lib64/libcudnn.so"), reason="Requires cudNN to be installed.")
 def test_clone_000003_05(darknet_root, clone_url):
-    # Build with GPU & cudNN
+    # Build with GPU & cudNN if available.
     dn = pydarknet2.Darknet(root=darknet_root)
     dn.build(gpu=True, cudnn=True, force=True)
     out = subprocess.check_output(["ldd", dn.exe]).decode("UTF-8")
